ddf94cf8684bdaf16a4b140f5d8691b2e0705f92
Jędrzej Kołtunowicz <jedrzej@ogarne.to>
2020-09-18 21:38:17 +0200

    Changed database behavior

--- src/main/java/to/ogarne/ogarneblog/config/SecurityConfig.java
+++ src/main/java/to/ogarne/ogarneblog/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
+        return new BCryptPasswordEncoder(12);
     }
 
     @Override
