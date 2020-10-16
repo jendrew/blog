a1fa5c5fa693e6d498b88332d97998494a822828
Jędrzej Kołtunowicz <jedrzej@gmail.com>
2020-10-16 20:59:21 +0200

    Changed deprecated thymeleaf syntax.

--- import.sql
+++ import.sql
@@ -1,7 +1,7 @@
 insert into role (name) values ('ROLE_ADMIN');
 insert into role (name) values ('ROLE_SUPERADMIN');
 
-insert into user (username, password, fullname, enabled, role_id, fbid) values ('jedrzej', '$2a$10$rsTfl7aSS4hWztBcpGBfLu4HJUP5Fb0bQqneXvQntJO6eowlomStu','Jędrzej K.',true, 0, 1327256238);
+insert into user (username, password, fullname, enabled, role_id, fbid) values ('jedrzej', '$2a$10$rsTfl7aSS4hWztBcpGBfLu4HJUP5Fb0bQqneXvQntJO6eowlomStu',true, 0, 1327256238);
 
 insert into category (name, displayName) values ('przyklad', 'przykład');
 
